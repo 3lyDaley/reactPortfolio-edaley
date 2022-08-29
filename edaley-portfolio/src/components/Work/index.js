60a4aea9b2596ba224e00dee56a25594dea2c1cb
E Daley <100460009+3lyDaley@users.noreply.github.com>
2022-08-28 22:50:41 -0700

    finsihed work page

--- edaley-portfolio/src/components/Work/index.js
+++ edaley-portfolio/src/components/Work/index.js
@@ -1,20 +1,41 @@
 import './index.scss'
-import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import Item from '@mui/material/Grid';
-const Work = () => {
+import homegrown from '../../assets/images/homgrown_screenshot.png'
+import mtg from '../../assets/images/mtg.png'
+import teamGen from '../../assets/images/teamGen.png'
+import workSch from '../../assets/images/SCREENSHOT2.png'
+
+
+function Work() {
+
+  
   return (
     <>
     <div className='work-page'>
+      <div className='grid'>
+        <div className='row1'>
+            
+        <a href="https://floating-island-17357.herokuapp.com/">
+        <img src={homegrown} alt="gardening site screenshot" width="400px"></img>
+        </a>
 
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          {Array.from(Array(6)).map((_, index) => (
-            <Grid item xs={2} sm={4} md={4} key={index}>
-              <Item>xs=2</Item>
-              
-            </Grid>
-          ))}
-        </Grid>
+            <a href="https://mtg-hub.herokuapp.com/">
+              <img src={mtg} alt="magic the gathering trade hub screenshot" height="260px" width="400px"></img>
+        </a>
+        </div>
+
+          <div className='row1'>
+            <a href="https://3lydaley.github.io/workScheduler/">
+              <img src={workSch} alt="workday scheduler screenshot" height="260px" width="400px"></img>
+            </a>
+
+            <a href="https://github.com/3lyDaley/TeamProfileGenerator">
+              <img src={teamGen} alt="runbuddy homepage screenshot" height="260px" width="400px"></img>
+            </a>
+          </div>
+
+      </div>
+    
+        
     </div>
     </>
   )
